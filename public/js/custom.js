fec5180537027a5b3e82b2148236bc169fa4fcae
incubated-geek-cc <xuemanxu.cc@gmail.com>
2022-12-03 21:23:00 +0800

    [Bug Fix] Read fileName by Save Button

--- public/js/custom.js
+++ public/js/custom.js
@@ -304,6 +304,7 @@ if (document.readyState === "complete" || document.readyState !== "loading" && !
       
       saveOutputBtn.addEventListener('click', async()=> {
         let dwnlnk = document.createElement('a');
+        let fileName = fileNameDisplay.innerHTML;
         let saveFilename = fileName.substr(0, fileName.lastIndexOf('.'));
         dwnlnk.download = `${saveFilename}${outputFileExt}`;
         dwnlnk.href = saveOutputBtn.value;
